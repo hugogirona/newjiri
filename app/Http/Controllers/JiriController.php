39b07378bbfa6bc70326f4e32f334b7f858fe62a
Hugo Girona <hugo.girona@student.hepl.be>
2025-10-19 15:20:29 +0200

    Added first files

---
+++ app/Http/Controllers/JiriController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class JiriController extends Controller
+{
+    //
+}
---
+++ app/Models/Jiri.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Jiri extends Model
+{
+    /** @use HasFactory<\Database\Factories\JiriFactory> */
+    use HasFactory;
+}
